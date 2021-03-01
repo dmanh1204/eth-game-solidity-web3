1915fae61b331e90a42099328f9a64b78b7175e2
manhdd3 <77711032+manhdd3@users.noreply.github.com>
2021-03-01 16:17:26 +0700

    update contract

--- src/Car.js
+++ src/Car.js
@@ -4,6 +4,7 @@ import colors from "./colors";
 
 const Car = ({ car }) => {
     const { id, name, color: _colors, level, ready} = car;
+    console.log(id, name, level, ready);
     const rect = ( _colors % 7) + 1;
     const front = ( _colors % 4) + 1;
     const hood = ( _colors % 5) + 1;
@@ -11,17 +12,22 @@ const Car = ({ car }) => {
     const f_hood = ( _colors % 2) + 1;
     const wheel_border = ( _colors % 4) + 1;
     return (
-        <div id="car">
-            <div class="rect" style={{ background: colors.rect[rect] }}></div>
-            <div class="front" style={{ background: colors.front[front] }}>
-                <div class="window" style={{ background: colors.window[window] }}></div>
-                <div class="f_hood" style={{ background: colors.f_hood[f_hood] }}></div>
+        <>
+            <div id="car">
+                <div class="rect" style={{ background: colors.rect[rect] }}></div>
+                <div class="front" style={{ background: colors.front[front] }}>
+                    <div class="window" style={{ background: colors.window[window] }}></div>
+                    <div class="f_hood" style={{ background: colors.f_hood[f_hood] }}></div>
+                </div>
+                <div class="tyres" style={{ background: colors.wheel_border[wheel_border] }}></div>
+                <div class="f_tyres" style={{ background: colors.wheel_border[wheel_border] }}></div>
+                <div class="ff_tyres" style={{ background: colors.wheel_border[wheel_border] }}></div>
+                <div class="hood" style={{ background: colors.hood[hood] }}></div>
             </div>
-            <div class="tyres" style={{ background: colors.wheel_border[wheel_border] }}></div>
-            <div class="f_tyres" style={{ background: colors.wheel_border[wheel_border] }}></div>
-            <div class="ff_tyres" style={{ background: colors.wheel_border[wheel_border] }}></div>
-            <div class="hood" style={{ background: colors.hood[hood] }}></div>
-        </div>
+            <div>
+                <button></button>
+            </div>
+        </>
     )
 }
 
